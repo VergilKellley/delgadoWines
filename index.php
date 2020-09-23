1cbe50587fa7a016ee239076c78ff3a534577a3e
Vergil Kelley <verg_1@yahoo.com>
2020-09-22 20:27:58 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  require 'includes/header.inc.php';
+?>
+
+<section class='hero'>
+    <div>
+        <p> Hero goes here</p>
+    </div>
+</section>
+
+<?php
+  require 'includes/footer.inc.php';
+?>
+    
