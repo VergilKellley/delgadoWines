543e1710311fcb1f6f1d15cd6f498da4b4ff1ee4
Vergil Kelley <verg_1@yahoo.com>
2020-09-23 13:16:55 -0500

    added button and text to hero

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
   require 'includes/header.inc.php';
 ?>
 
-<section class='hero'>
-    <div>
-        <p> Hero goes here</p>
+<section class='hero-container'>
+    <div class="hero-image-container">
+      <div class="hero-text">
+        <h1>Divi Wines</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime beatae fugit molestias aliquam numquam quae autem, velit ea doloremque, reprehenderit atque voluptates modi magni amet dignissimos obcaecati ullam debitis!</p>
+        <button class="buttons" id="browseWinesBtn" type="submit">Browse Our Wines</button>     
+      </div>
+        <img src="images/winebottle1.png" alt="wine bottle" />
     </div>
 </section>
 
