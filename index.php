f5f234458ab3bc4683901baaec9b99bf5132e8eb
Vergil Kelley <verg_1@yahoo.com>
2020-09-25 15:47:35 -0500

    added text and images to sections 4 5 and 6

--- index.php
+++ index.php
@@ -26,9 +26,75 @@
       </div>
       <button id="more-about-us-btn" class="buttons" type="submit">More About Us</button>
     </div>
-    
   </div>
 </section>
+<section class="third-container">
+  <div class="third-container-content">
+    <h2>Our Wines</h2>
+    <div class="our-wines-imgs">
+      <img src="images/wine5.png" alt="bottle of wine" />
+      <img src="images/wine6.png" alt="bottle of wine" />
+      <img src="images/wine7.png" alt="bottle of wine" />
+    </div>
+    <button class="buttons" type="submit">Browse Our Wines</button>
+  </div>
+</section>
+<section class="fourth-container">
+  <div class="fourth-container-content">
+    <div class="fourth-img-overlay-container">
+      <div class="fourth-container-bg-img-3"></div>
+      <div class="fourth-container-img-overlay"></div>
+      <h2 class="overlay-text">Wine</h2>
+    </div>
+    <div class="fourth-img-overlay-container">
+      <div class="fourth-container-bg-img-2"></div>
+      <div class="fourth-container-img-white-overlay"></div>
+      <h2 class="overlay-text">Snacks</h2>
+    </div>
+    <div class="fourth-img-overlay-container">
+      <div class="fourth-container-bg-img-1"></div>
+      <div class="fourth-container-img-overlay"></div>
+      <h2 class="overlay-text">Shop</h2>
+    </div>
+  </div>
+</section>
+<section class="fifth-container">
+  <h2>Our Process</h2>
+  <div class="fifth-container-content">
+    <div class="process-sections">
+      <div class="process-box">
+        <h3>1. Harvesting</h3>
+        <h3>2. Crushing</h3>
+        <h3>3. Fermentation</h3>
+        <h3>4. Clarification</h3>
+        <h3>5. Aging and bottling</h3>
+        <button class="buttons" type="submit">Learn More</button>
+      </div>
+      <div class="process-text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vitae necessitatibus obcaecati nulla omnis est delectus sapiente sed ad odio voluptates earum sit, quidem harum, dicta non impedit qui animi unde minima temporibus expedita nostrum? In assumenda officia cupiditate architecto!</p></br>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo? Quaerat, quae hic ea ipsam sit exercitationem sapiente ut fugit quos quas aspernatur. Iste.</p></br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quas magni, ad voluptate excepturi consequuntur sit nesciunt.</p>
+      </div>
+      <div class="process-text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p></br>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p></br>
+        <img src="images/wine11.png" alt="wine glass"/>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="sixth-container">
+  <div class="sixth-container-content">
+    <div id="fifty-k-container">
+      <h2 id="fifty-k"><span id="fifty-k-text">50k</span><span id="fifty-k-bottles-per-year"> Bottles Per Year</span></h2>
+    </div>
+    <div id="twenty-twenty-container">
+      <h2 id="twenty-twenty">2020</h2>
+      <img src="images/wine12.png" alt="bottle of wine"/>
+    </div>
+  </div>
+</section>
+<div></div>
 
 <?php
   require 'includes/footer.inc.php';
