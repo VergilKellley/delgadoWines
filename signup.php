c757e90ac923320a5674432a054fbca041c3982f
Vergil Kelley <verg_1@yahoo.com>
2020-09-30 19:11:34 -0500

    added classes

--- signup.php
+++ signup.php
@@ -55,7 +55,7 @@
             </div>
         </div>
     </section>
-    <section class="fourth-container">
+    <section class="fourth-container-signup-page">
         <div class="bottle-club-container">
             <div class="bottle-club">
                 <h2>4 Bottle Club</h2>
@@ -80,7 +80,30 @@
             </div>
         </div>
     </section>
-    
+    <section class="fifth-container-signup-page">
+        <img src="images/wine20.jpg" alt='wine bottle and wine glass' />
+    </section>
+    <section class="ninth-container">
+  <div class="ninth-container-bg-image"></div>
+  <div class="ninth-container-visit-Us-content">
+    <h2>Come Visit Us</h2>
+    <button class="buttons" type="submit">Plan Your Visit</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sit eligendi beatae est recusandae quod?</p>
+  </div>
+  <div class="phone-address-box">
+    <p><strong>Phone</strong></p>
+    <br>
+    <p>(555) 555-5555</p>
+    <br><br>
+    <p><strong>Address</strong></p>
+    <br>
+    <p> 1234 N. Clark Street<br>
+        Some City Illinois 60601</p>
+        <br><br>
+    <h2>Join Our Wine Club</h2>
+    <button class="wine-club-btn-2" type="submit">Learn More</button>
+    <div>
+</section>  
 <?php
     require "includes/footer.inc.php";
 ?>
