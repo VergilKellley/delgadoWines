81c8c95872db39023ea3aaf927052247292a5abc
Vergil Kelley <verg_1@yahoo.com>
2020-10-06 21:12:15 -0500

    added text to first container shop page

--- shop.php
+++ shop.php
@@ -1,9 +1,16 @@
 <?php
   require 'includes/header.inc.php';
 ?>
-<div>
-    <h2>Coming Soon</h2>
-</div>
+<section class="first-shopping-page-container">
+  <div class="shop-online-text">
+    <h2>Shop</h2><h2>Online</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique quam eum ipsam voluptates libero sunt voluptas, obcaecati iusto voluptatum, quis ea molestias tenetur nobis accusamus. Error ipsum corrupti aut? Laborum.</p>
+  </div>
+  <div class="shop-online-box">
+    <h3>Join Our<br> Wine Club</h3>
+  </div>
+
+</section>
 
 <?php
   require 'includes/footer.inc.php';
