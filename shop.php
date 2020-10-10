cb6fc166c33b84b330e1a4b83ec80d9523bcd20a
Vergil Kelley <verg_1@yahoo.com>
2020-10-10 01:56:15 -0500

    added image

--- shop.php
+++ shop.php
@@ -11,7 +11,23 @@
     <button class='wine-club-btn-2' type='submit'>Learn More</button>
   </div>
   <div id="join-bg-image"></div>
-
+</section>
+<section class="second-shopping-page-container">
+  <div class="year-buttons-container">
+    <button class="year-buttons" type='submit'>2012</button>
+    <button class="year-buttons" type='submit'>2013</button>
+    <button class="year-buttons" type='submit'>2014</button>
+    <button class="year-buttons" type='submit'>2015</button>
+    <button class="year-buttons" type='submit'>2016</button>
+    <button class="year-buttons" type='submit'>2017</button>
+  </div>
+  <div class="vintage-text-container">
+    <h2>2020 Vintage</h2>
+  </div>
+  <div id="twenty-twenty-container">
+      <h2 id="twenty-twenty">2020</h2>
+      <img src="images/wine12.png" alt="bottle of wine"/>
+    </div>
 </section>
 
 <?php
