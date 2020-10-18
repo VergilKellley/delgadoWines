26c6e7b677d5d7ccda6c695aef232c4b61d7abdb
Vergil Kelley <verg_1@yahoo.com>
2020-10-18 14:34:04 -0500

    added bg image

--- about.php
+++ about.php
@@ -1,9 +1,16 @@
 <?php
   require 'includes/header.inc.php';
 ?>
-<div>
-    heyyyyy
-</div>
+<section class="first-about-page-container">
+<div class="about-page-background-image"></div>
+<div class="blurr-background"></div>
+  <div class="about-winery-container">
+    <h2>About Our Winery</h2>
+    <img class="about-page-grapes-image" src="images/winery29.png" alt="wine grapes"/>
+    <p>Making Wine Since</p>
+    <h2>1950</h2>
+  </div>
+</section>
 
 <?php
   require 'includes/footer.inc.php';
