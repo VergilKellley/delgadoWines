4178df7409fa1b9154b206ab6b90fefebf5e3fcd
Vergil Kelley <verg_1@yahoo.com>
2020-10-20 17:48:50 -0500

    added second section

--- about.php
+++ about.php
@@ -14,6 +14,15 @@
     </div>
   </div>
 </section>
+<section class="second-about-page-container">
+  <div class='about-page-img-text-container'>
+    <img src="images/winery41.png" alt="wine bottle and glass"/>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus perferendis facere magnam ullam molestiae voluptas architecto excepturi ducimus omnis laborum, temporibus nobis, saepe, nisi praesentium dolor esse. Velit natus quaerat et! Non, sit aspernatur illum incidunt eligendi dignissimos delectus quisquam nesciunt nulla libero dolorum maxime repudiandae iste mollitia? Obcaecati, ducimus.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, iusto amet ipsum placeat eligendi doloremque veniam maiores fugiat. Minima quos, cumque expedita sapiente neque ullam quibusdam voluptates dolorum earum animi magnam voluptas vero maiores asperiores numquam, sed, officiis accusantium nulla repellat modi. Doloremque, enim placeat atque aliquam officia cupiditate eos neque nam libero cum repellat.
+    </p>
+  </div>
+</section>
 
 <?php
   require 'includes/footer.inc.php';
