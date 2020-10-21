77d49bc00107f4ed05355550595ff7701e5f88e2
Vergil Kelley <verg_1@yahoo.com>
2020-10-21 18:21:23 -0500

    added fourth section to about page

--- about.php
+++ about.php
@@ -17,10 +17,51 @@
 <section class="second-about-page-container">
   <div class='about-page-img-text-container'>
     <img src="images/winery41.png" alt="wine bottle and glass"/>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus perferendis facere magnam ullam molestiae voluptas architecto excepturi ducimus omnis laborum, temporibus nobis, saepe, nisi praesentium dolor esse. Velit natus quaerat et! Non, sit aspernatur illum incidunt eligendi dignissimos delectus quisquam nesciunt nulla libero dolorum maxime repudiandae iste mollitia? Obcaecati, ducimus.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, iusto amet ipsum placeat eligendi doloremque veniam maiores fugiat. Minima quos, cumque expedita sapiente neque ullam quibusdam voluptates dolorum earum animi magnam voluptas vero maiores asperiores numquam, sed, officiis accusantium nulla repellat modi. Doloremque, enim placeat atque aliquam officia cupiditate eos neque nam libero cum repellat.
-    </p>
+    <div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus perferendis facere magnam ullam molestiae voluptas architecto excepturi ducimus omnis laborum, temporibus nobis, saepe, nisi praesentium dolor
+      esse. Velit natus quaerat et!
+      <br /> 
+      <br /> 
+      Non, sit aspernatur illum incidunt eligendi dignissimos delectus quisquam nesciunt nulla libero dolorum maxime repudiandae iste mollitia? Obcaecati, ducimus.</p>
+    </div>
+    <div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus perferendis facere magnam ullam molestiae voluptas architecto excepturi ducimus omnis laborum, temporibus nobis, saepe, nisi praesentium dolor
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, iusto amet ipsum placeat eligendi doloremque veniam maiores fugiat. 
+      <br /> 
+      <br /> 
+      Minima quos, cumque expedita sapiente neque ullam quibusdam voluptates dolorum earum animi magnam voluptas vero maiores asperiores numquam, sed, officiis accusantium nulla repellat modi. Doloremque, enim placeat.</p>
+    </div>
+  </div>
+</section>
+<section class="third-about-page-container">
+  <div class="about-page-third-container-content">
+    <div>
+      <h2>The Art of <br />Wine Making</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, distinctio. Provident quaerat pariatur repellendus molestiae laboriosam eum quidem voluptas veniam praesentium rem. Enim corrupti praesentium quibusdam iure culpa eveniet optio!</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, distinctio. Provident quaerat pariatur repellendus molestiae laboriosam eum quidem voluptas veniam praesentium rem. Enim corrupti praesentium quibusdam iure culpa eveniet optio!</p>
+    </div>
+    <div>
+      <img src="images/wine11.png" alt="wine bottle" />
+    </div>
+  </div>
+</section>
+<section class="fourth-about-page-container">
+  <div class="about-page-box-text-container">
+    <img src="images/winery42.png" alt="wine bottle">
+    <div class="about-page-box">
+      <h3>Wine Tastings</h3>
+      <p>7 days a week from 1pm - 5pm Prescheduled private tastings available</p>
+      <h3>Winery Tours</h3>
+      <h3>By Appointment Only</h3>
+   </div>
+   <div class="about-page-fiftyK-text">
+     <h2>50k</h2>
+     <span>Bottles</span>
+     <p>Produced / Yr </p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quam quasi perspiciatis qui quod, laborum, consequatur quia numquam voluptatem nesciunt autem excepturi quidem possimus quas?</p>
+     <button class="" type="submit" >Visit Our Winery</button>
+   </div>
   </div>
 </section>
 
