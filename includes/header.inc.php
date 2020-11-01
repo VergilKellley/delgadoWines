90ad945eaab058e367edd405219b45c18b5fd9a3
Vergil Kelley <verg_1@yahoo.com>
2020-10-31 23:59:57 -0500

    added text

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -33,7 +33,7 @@
         </div>
         <ul>
           <li><a href="index.php">Home</a></li>
-          <li><a href="shop.php">Shop</a></li>
+          <li><a href="shop.php">Shoping</a></li>
           <li><a href="about.php">About</a></li>
           <li><a href="contact.php">Contact</a></li>
           <?php
