dfd3feb08a3f30b4991dd08630578a44809faf1b
Vergil Kelley <verg_1@yahoo.com>
2020-11-01 22:39:22 -0600

    changed nav text

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -33,8 +33,8 @@
         </div>
         <ul>
           <li><a href="index.php">Home</a></li>
-          <li><a href="shop.php">Shoping</a></li>
-          <li><a href="about.php">About</a></li>
+          <li><a href="shop.php">Shop</a></li>
+          <li><a href="about.php">About Us</a></li>
           <li><a href="contact.php">Contact</a></li>
           <?php
           if (isset($_SESSION['userId'])) {
