35ed946a1f9a581dc7d54ed039d22921f10c2424
Vergil Kelley <verg_1@yahoo.com>
2020-11-12 19:55:37 -0600

    added text to nav

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -35,7 +35,7 @@
           <li><a href="index.php">Home</a></li>
           <li><a href="shop.php">Shop</a></li>
           <li><a href="about.php">About</a></li>
-          <li><a href="contact.php">Contact</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
           <?php
           if (isset($_SESSION['userId'])) {
             echo '
