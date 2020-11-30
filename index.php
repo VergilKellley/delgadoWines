198d4a1f50a150f9fe4c90ebc1b29d0b950cc32a
Vergil Kelley <verg_1@yahoo.com>
2020-11-29 18:54:23 -0600

    added class to sec 5

--- index.php
+++ index.php
@@ -1,132 +1,179 @@
-<?php
-  require 'includes/header.inc.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css" />
+    <title>Winery Home Page</title>
+</head>
+<body>
+    <section id='first-sec-container'>
+        <div id="first-sec-text-container">
+            <h1>Elegant Winery</h1>
+            <p>Lorem ipsum dolor sit mowhbvo amet consectetur  mowhbvo amet consectetu adipisicing elit. Distinctio maxime beatae fugit molestias aliquam numquam quae autem, velit ea doloremque, reprehenderit atque voluptates modi magni amet dignissimos obcaecati ullam debitis! ipsum dolor sit mowhbvo</p>
+            <button class="large-buttons buttons" id="browseWinesBtn" type="submit">Browse Our Wines</button>     
+        </div>
+        <div id="first-sec-image-container">
+            <img src="images/winebottle1.png" alt="wine bottle" />
+        </div>
+    </section>
+    <section class="second-container">
+          <div id="second-sec-image-container">
+              <img src="images/wine2.jpg" alt="three wine glasses" />
+          </div>
+          <div id="boxes-container">
+            <div id="boxes-btn-container">
+            <div class="boxes">
+                <div class="box" id="box-1">
+                <h3><strong>Lorem ipsum dolor.</strong></h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat.</p>
+                </div>
+                <div class="box" id="box-2">
+                <h3><strong>Sed porttitor lectus nibh</strong></h3>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+             </div>
+            </div>
+            <button class="buttons large-buttons boxes-btn" type="submit">More About Us</button>
+          </div>
+      </section>
+      <section id="third-container">
+        <div id="our-wines-content">
+          <h2>Our Wines</h2>
+          <div id="our-wines-imgs">
+            <img src="images/wine5.png" alt="bottle of wine" />
+            <img src="images/wine6.png" alt="bottle of wine" />
+            <img src="images/wine7.png" alt="bottle of wine" />
+          </div>
+          <button class="buttons large-buttons" type="submit">Browse Our Wines</button>
+        </div>
+      </section>
+      <section id="fourth-container">
 
-<section class='hero-container'>
-    <div class="hero-image-container">
-      <div class="hero-text">
-        <h1>Elegant Winery</h1>
-        <p>Lorem ipsum dolor sit mowhbvo amet consectetur  mowhbvo amet consectetu adipisicing elit. Distinctio maxime beatae fugit molestias aliquam numquam quae autem, velit ea doloremque, reprehenderit atque voluptates modi magni amet dignissimos obcaecati ullam debitis! ipsum dolor sit mowhbvo</p>
-        <button class="buttons" id="browseWinesBtn" type="submit">Browse Our Wines</button>     
-      </div>
-      <div>
-        <img src="images/winebottle1.png" alt="wine bottle" />
-      </div>
-    </div>
-</section>
-<section class="second-container">
-  <div class="second-container-contents">
-    <div class="three-wine-glasses"></div>
-    <div class="boxes">
-      <div class="box" id="box-1">
-        <h3><strong>Lorem ipsum dolor sit.</strong></h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat.</p>
-      </div>
-      <div class="box" id="box-2">
-        <h3><strong>Lorem, ipsum dolor.</strong></h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat.</p>
-      </div>
-    </div>
-    <button id="more-about-us-btn" class="buttons" type="submit">More About Us</button>
-  </div>
-</section>
-<section class="third-container">
-  <div class="third-container-content">
-    <h2>Our Wines</h2>
-    <div class="our-wines-imgs">
-      <img src="images/wine5.png" alt="bottle of wine" />
-      <img src="images/wine6.png" alt="bottle of wine" />
-      <img src="images/wine7.png" alt="bottle of wine" />
-    </div>
-    <button class="buttons" type="submit">Browse Our Wines</button>
-  </div>
-</section>
-<section class="fourth-container">
-  <div class="fourth-container-content">
-    <div class="fourth-img-overlay-container">
-      <div class="fourth-container-bg-img-3"></div>
-      <div class="fourth-container-img-overlay"></div>
-      <h2 class="overlay-text">Wine</h2>
-    </div>
-    <div class="fourth-img-overlay-container">
-      <div class="fourth-container-bg-img-2"></div>
-      <div class="fourth-container-img-white-overlay"></div>
-      <h2 class="overlay-text">Snacks</h2>
-    </div>
-    <div class="fourth-img-overlay-container">
-      <div class="fourth-container-bg-img-1"></div>
-      <div class="fourth-container-img-overlay"></div>
-      <h2 class="overlay-text">Shop</h2>
-    </div>
-  </div>
-</section>
-<section class="fifth-container">
-  <h2>Our Process</h2>
-  <div class="fifth-container-content">
-    <div class="process-sections">
-      <div class="process-box">
-        <h3>1. Harvesting</h3>
-        <h3>2. Crushing</h3>
-        <h3>3. Fermentation</h3>
-        <h3>4. Clarification</h3>
-        <h3>5. Aging and bottling</h3>
-        <button class="buttons" type="submit">Learn More</button>
-      </div>
-      <div class="process-text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vitae necessitatibus obcaecati nulla omnis est delectus sapiente sed ad odio voluptates earum sit, quidem harum, dicta non impedit qui animi unde minima temporibus expedita nostrum? In assumenda officia cupiditate architecto!</p></br>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo? Quaerat, quae hic ea ipsam sit exercitationem sapiente ut fugit quos quas aspernatur. Iste.</p></br>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quas magni, ad voluptate excepturi consequuntur sit nesciunt.</p>
-      </div>
-      <div class="process-text">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p></br>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p></br>
-        <img src="images/wine11.png" alt="wine glass"/>
-      </div>
-    </div>
-  </div>
-</section>
-<section class="sixth-container">
-  <div class="sixth-container-content">
-    <div id="fifty-k-container">
-      <h2 id="fifty-k"><span id="fifty-k-text">50k</span><span id="fifty-k-bottles-per-year"> Bottles Per Year</span></h2>
-    </div>
-    <div id="twenty-twenty-container">
-      <h2 id="twenty-twenty">2020</h2>
-      <img src="images/wine50.png" alt="bottle of wine"/>
-    </div>
-    <div class="shop-vintage-btn">
-      <button class="buttons" type="submit">Shop 2020 Vintage</button>
-    </div>
-  </div>
-</section>
-<section class="seventh-container">
-  <div class="seventh-container-content">
-    <div class="reviews-text">
-      <img src="images/winer14.jpg" alt="wine glass and corkscrew" />
-      <p>AWLorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem eveniet iusto atque deleniti consequatur et dolore nostrum architecto fugiat nam!<p>
-    </div>
-  </div>
-</section>
-<section class="eigth-container">
-  <div class="eigth-container-content">
-    <div class="eigth-container-overlay"></div>
-    <div class="eigth-container-bg-image"></div>
-  </div>
-  <div class="wine-club-container">
-    <div class="wine-club-box-content">
-      <h2 id="join-wine-club">Join Our Wine Club</h2>
-      <button class="wine-club-btn" type="submit">Learn More</button>
-      <p><strong>Lorem, ipsum dolor sit amet</strong> consectetur adipisicing elit. Quibusdam porro quam numquam eligendi asperiores. Nesciunt.</p>
-    </div>
-    <div class="wine-club-subscription-content">
-      <h2>Subscribe</h2>
-      <input type="text" name="subscription" placeholder="Email" />
-      <button class="wine-club-submit-btn" type="submit">Subscribe</button>
-    </div>
-  </div>
-</section>
+        <div id="fourth-container-content">
+          <div class="fourth-img-overlay-container">
+            <div class="fourth-container-img-overlay"></div>
+            <h2 class="overlay-text">Wine</h2>
+            <div class="fourth-bg-img" id="fourth-container-bg-img-3"></div>
+          </div>
 
+          <div class="fourth-img-overlay-container">
+            <div class="fourth-container-img-white-overlay"></div>
+            <h2 class="overlay-text">Snacks</h2>
+            <div class="fourth-bg-img" id="fourth-container-bg-img-2"></div> 
+          </div>
+
+          <div class="fourth-img-overlay-container">
+            <div class="fourth-container-img-overlay"></div>
+            <h2 class="overlay-text">Shop Now</h2>
+            <div class="fourth-bg-img" id="fourth-container-bg-img-1"></div>  
+          </div>
+        </div>
+      </section>
+      <section id="fifth-container">
+        <div id="fifth-container-content">
+            <h2>Our Process</h2>
+            
+          <div id="process-sections">
+            <div id="process-section-content">
+            <div id="process-box">
+              <div id="process-box-content">
+                <h3>1. Harvesting</h3>
+                <h3>2. Crushing</h3>
+                <h3>3. Fermentation</h3>
+                <h3>4. Clarification</h3>
+                <h3>5. Aging & Bottling</h3>
+              </div>
+                <div id="process-btn-container">
+                <button class="buttons large-buttons process-box-btn" type="submit">Learn More</button>
+                </div>
+              </div>
+            
+            <div class="process-text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vitae necessitatibus obcaecati nulla omnis est delectus sapiente sed ad odio voluptates earum sit, quidem harum, dicta non impedit qui animi unde minima temporibus expedita nostrum? In assumenda officia cupiditate architecto!</p>
+              <br/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo? Quaerat, quae hic ea ipsam sit exercitationem sapiente ut fugit quos quas aspernatur. Iste.</p>
+              <br/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quas magni, ad voluptate excepturi consequuntur sit nesciunt.</p>
+            </div>
+            
+            <div class="process-text">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p>
+              <br/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error eum blanditiis, vero suscipit temporibus ad, dolorum vitae iure repellendus possimus laborum nemo voluptatibus minima! Quo?</p>
+              <img src="images/wine11.png" alt="wine glass"/>
+            </div>
+          </div>
+          </div>
+        </div>
+      </section>
+      <section class="sixth-container">
+        <div class="sixth-container-content">
+          <div id="fifty-k-container">
+            <h2 id="fifty-k"><span id="fifty-k-text">50k</span><span id="fifty-k-bottles-per-year"> Bottles Per Year</span></h2>
+          </div>
+          <div id="twenty-twenty-container">
+            <h2 id="twenty-twenty">2020</h2>
+            <img src="images/wine50.png" alt="bottle of wine"/>
+          </div>
+          <div id="shop-vintage-btn">
+            <button class="buttons large-buttons" type="submit">Shop 2020 Vintage</button>
+          </div>
+        </div>
+      </section>
+      <section id="seventh-container">
+        <div id="seventh-container-content">
+          <div id="reviews-text">
+            <img src="images/winer14.jpg" alt="wine glass and corkscrew" />
+            <p>AWLorem ipsum dolor, sit amet consectetur adipisicing elit.<p>
+          </div>
+        </div>
+      </section>
+      <section id="eigth-container">
+        <div id="eigth-container-content">       
+          <img id="eigth-container-image" src="images/wine15.jpg" alt="cheese and crackers" >
+          <div id="eigth-container-overlay"></div>
+        </div>
+        <div id="wine-club-container">
+          <div id="wine-club-box-content">
+            <h2 id="join-wine-club">Join Our Wine Club</h2>
+            <a class="wine-club-btn small-buttons" href="#" type="submit">Learn More</a>
+            <!--<p><strong>Lorem, ipsum dolor sit amet</strong> consectetur adipisicing elit. Quibusdam porro quam numquam eligendi asperiores. Nesciunt.</p>-->
+          </div>
+          <div id="wine-club-subscription-content">
+            <h2>Subscribe</h2>
+            <input type="text" name="subscription" placeholder="Email" /><br/>
+            <button class="wine-club-submit-btn" type="submit">Subscribe</button>
+          </div>
+        </div>
+        </section>
+
+
+        <section id="eigth-container-mobile">
+        <div id="eigth-container-content-mobile">       
+          <img id="eigth-container-image-mobile" src="images/wine15.jpg" alt="cheese and crackers" >
+          <div id="eigth-container-overlay-mobile"></div>
+        </div>
+        <div id="wine-club-container-mobile">
+          <div id="wine-club-box-content-mobile">
+            <h2 id="join-wine-club-mobile">Join Our Wine Club</h2>
+            <a class="wine-club-btn-mobile small-buttons" href="#" type="submit">Learn More</a>
+          </div>
+
+
+          <div id="wine-club-subscription-content-mobile">
+            <h2>Subscribe</h2>
+            <input type="text" name="subscription" placeholder="Email" /><br/>
+            <button class="wine-club-submit-btn-mobile" type="submit">Subscribe</button>
+          </div>
+          <!--<div id="wine-club-text-mobile">
+          <div id="wine-club-text-content-mobile">
+          <p><strong>Lorem, ipsum dolor sit amet</strong> consectetur adipisicing elit. Quibusdam porro quam numquam eligendi asperiores nesciunt.</p>
+          </div>-->
+          </div>
+        </div>
+      </section>
+</body>
+</html>
 <?php
   require 'includes/footer.inc.php';
-?>
-    
+  ?>
