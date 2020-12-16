ea72bd30bed517a529e8d5f1d80ab8bcd695ae0e
Vergil Kelley <verg_1@yahoo.com>
2020-12-15 21:05:37 -0600

    changed text

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
     <img src="images/winery41.png" alt="wine bottle and glass"/>
     <div>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus perferendis facere magnam ullam molestiae voluptas architecto excepturi ducimus omnis laborum, temporibus nobis, saepe, nisi praesentium dolor
-      esse. Velit natus quaerat et!
+      esse. Velit natus quaerat  consectetur adipisicinet!
       <br /> 
       <br /> 
       Non, sit aspernatur illum incidunt eligendi dignissimos delectus quisquam nesciunt nulla libero dolorum maxime repudiandae iste mollitia? Obcaecati, ducimus.</p>
